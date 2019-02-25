f88a2e0f486d62c7af803dead23829b1c18fa8f2
Shovan Swain <shovanswain@gmail.com>
2019-02-25 18:35:26 +0530

    added simulation

---
+++ sim.py
@@ -0,0 +1,26 @@
+from environment import Environment
+import numpy as np
+
+environment = Environment()
+q_table = np.loadtxt("qtable.txt")
+
+s0 = environment.state
+
+totalReward = 0
+while(totalReward >= -1000):
+	print(s0)
+	stateNumber = (4**3)*(s0["position"]-1) + (4**2)*(s0["truck1_inventory"]) + (4**1)*(s0["shop1_inventory"]) + s0["shop2_inventory"]
+	action = np.argmax(q_table[stateNumber])
+
+	actionStr = ""
+	for stri, number in environment.actions.items():
+		if action == number:
+			actionStr =  stri
+
+	action = actionStr
+
+	reward = environment.perform_action(environment.actions[action])
+	totalReward+=reward
+	s0 = environment.state
+
+	print(action, totalReward, "\n")
